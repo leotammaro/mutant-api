3a84f6c9078d1cc0ef59756aa56c55805e5906e4
Leonel Tammaro <leoneltammaro@MacBook-Pro-de-Leonel.local>
2024-11-10 20:27:58 -0300

    Init api config

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+    }),
+  ],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ src/common/orm.configuration.ts
@@ -0,0 +1,22 @@
+import { join } from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+export const ormConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT!,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+};
+
+export default new DataSource({
+  ...ormConfig,
+  entities: [
+    join(__dirname, '../modules/**/infrastructure/database/*.entity.ts'),
+  ],
+  migrations: ['./data/migrations/*.ts'],
+});
